357724d2cc54b300227284a0296285751508d450
amb125 <anbaba508@gmail.com>
2024-10-18 17:22:33 +0200

    created destination.json file and added map to show destination name and img to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import destinations from "./destinations.json";
+import React, {useState, useEffect} from 'react';
+
+// Now that you’ve imported the destination data, save it in a state variable using useState. Map over the state variable and render a simple list that displays the destination names and images:
+
+// ```
+// <div>
+//   <p>DESTINATION_NAME_HERE</p>
+//   <img src="DESTINATION_IMAGE_HERE" width={100} />
+// </div>
+// ```
+
 
 function App() {
+  const [destinationData, setDestinationData] = useState([])
+  // console.log(destinationData);
+
+  useEffect(() => {setDestinationData(destinations)}, [])
+  console.log(destinationData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {destinationData.map((destination) => (
+        <>
+          <p>{destination.name}</p>
+          <img src={destination.image} width={100} />
+        </>
+      )
+      )}
+    </>
   );
 }
 
